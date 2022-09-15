02d20fee6a8bf848c193aa33a0e1015f4d315145
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-09-16 01:37:44 +0500

    doing some more code in BasicNavigation class

--- src/test/java/day01_selenium_intro/BasicNavigations.java
+++ src/test/java/day01_selenium_intro/BasicNavigations.java
@@ -3,25 +3,46 @@ package day01_selenium_intro;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BasicNavigations {
     public static void main(String[] args) throws InterruptedException{
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("lang=en-GB");
+
         WebDriverManager.chromedriver().setup();
 
-        WebDriver webDriver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
 
-        webDriver.get("https://www.google.com");
-        Thread.sleep(5000);
-        webDriver.navigate().to("https://www.tesla.com");
-        Thread.sleep(5000);
-        webDriver.navigate().back();
-        Thread.sleep(5000);
-        webDriver.navigate().forward();
-        Thread.sleep(5000);
-        webDriver.navigate().refresh();
+        driver.manage().window().maximize();
 
-        webDriver.close(); // closing only the current page
+        driver.get("https://www.google.com");
+//        Thread.sleep(1000);
+        driver.navigate().to("https://jut.su/anime/");
+        String currentTitle = driver.getTitle();
+        System.out.println("currentTitle = " + currentTitle);
 
-        // if there are several pages on the browsers, webDriver.quit() methods will close all of them
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("currentUrl = " + currentUrl);
+//        Thread.sleep(1000);
+        driver.navigate().back();
+//        Thread.sleep(1000);
+        driver.navigate().forward();
+//        Thread.sleep(1000);
+        driver.navigate().refresh();
+
+
+        driver.navigate().to("https://www.google.com");
+        currentTitle = driver.getTitle();
+        System.out.println("title = " + currentTitle);
+//      System.out.println(driver.getTitle());
+
+        currentUrl = driver.getCurrentUrl();
+        System.out.println("currentUrl = " + currentUrl);
+
+
+        //driver.close(); // closing only the current page
+        // if there are several pages on the browsers, driver.quit() methods will close all of them
+        driver.quit();
     }
 }
