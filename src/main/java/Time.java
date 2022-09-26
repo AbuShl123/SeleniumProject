ea2b963f0825322220f7e3ce5d0aee278fdbc279
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-09-26 10:24:48 +0500

    new classes in prctice package

---
+++ src/main/java/Time.java
@@ -0,0 +1,9 @@
+public class Time {
+    public static void main(String[] args) {
+        args[0] = "sun";
+        args[1] = "moon";
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+    }
+}
