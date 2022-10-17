a455e2b74aeac861c74e05e9f57c971856183c17
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-10-17 19:44:33 +0500

    driver and singleton

---
+++ src/test/java/test/day10_javafaker_driver_util/Old_driver_test.java
@@ -0,0 +1,40 @@
+package test.day10_javafaker_driver_util;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.Driver;
+import utilities.WebDriverFactory;
+
+
+import java.util.concurrent.TimeUnit;
+
+
+
+public class Old_driver_test {
+    WebDriver driver;
+    @BeforeMethod
+    public void setUpMethod(){
+        // set up chrome and create WebDriver instance
+        driver = Driver.getDriver();
+    }
+
+    @Test
+    public void googleTitle(){
+        driver.get("https://google.com");
+        System.out.println("google test driver = " + ((RemoteWebDriver) driver).getSessionId());
+    }
+
+    @Test
+    public void yahooTitle(){
+        driver.get("https://yahoo.com");
+        System.out.println("yahoo test driver = " + ((RemoteWebDriver) driver).getSessionId());
+    }
+
+    @Test
+    public void etsyTitle(){
+        driver.get("https://etsy.com");
+        System.out.println("etsy test driver = " + ((RemoteWebDriver) driver).getSessionId());
+    }
+
+}
---
+++ src/test/java/test/day10_javafaker_driver_util/Singleton.java
@@ -0,0 +1,17 @@
+package test.day10_javafaker_driver_util;
+
+public class Singleton {
+    private static String word;
+
+    private Singleton(){}
+
+    public static String getWord(String newWord) {
+        if (word == null) {
+            System.out.println("First time call. Word object is null. Assigning the value now");
+           word = newWord;
+        } else {
+            System.out.println("Word already has value");
+        }
+        return word;
+    }
+}
